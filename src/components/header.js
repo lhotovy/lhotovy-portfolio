7a5623d3dc7af18d9c41f2afc2a00bb755cc4093
lhotovy <lhotovy@centrum.cz>
2023-06-12 15:25:04 +0200

    Deleted database

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,10 @@
-import { LikeButton } from "./likeButton"
 import { SocialMedia } from "./socialMedia"
 import { ToggleButton } from "./toggleButton"
 
 
 export const Header = (props) => {
   return (
-    <div className="flex justify-around px-16 h-16 w-2/3 mx-auto">
-        <LikeButton />
+    <div className="flex justify-around px-16 h-16 w-2/3 mx-auto"> 
         <SocialMedia />
         <ToggleButton onClick={props.onClick} theme={props.theme} />
     </div>
--- src/components/sideNav.js
+++ src/components/sideNav.js
@@ -12,9 +12,9 @@ export const SideBar = () => {
             <div className={`bg-sky-800 h-screen p-5 pt-8 ${open ? "w-64" : "w-20"} duration-300 absolute hidden md:inline-flex`}>
                 <BsArrowLeftShort className={`bg-white text-sky-800 text-3xl rounded-full absolute -right-3 top-6 border border-sky-800 cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
                 <ul className="pt-6">
-                    {navs.map((nav, id) => (
+                    {navs.map((nav) => (
                         <>
-                            <li key={id} className="text-white hover:bg-slate-700 text-sm flex justify-start items-center gap-x-4 cursor-pointer p-2 rounded-md mt-2">
+                            <li key={nav.id} className="text-white hover:bg-slate-700 text-sm flex justify-start items-center gap-x-4 cursor-pointer p-2 rounded-md mt-2">
                                 <Link to={nav.link}>
                                     <i className={`${nav.icon} inline-flex justify-center`}/>
                                     <span className={`${!open && "hidden"} inline-flex justify-start flex-1 font-medium tracking-wider ml-4`}>{nav.name}</span>
@@ -26,9 +26,9 @@ export const SideBar = () => {
             </div>
             <div className={`bg-sky-800 w-screen p-3 fixed z-10 bottom-0 md:hidden flex flex-row justify-center`}>
                 <ul className="flex flex-row">
-                    {navs.map((nav, id) => (
+                    {navs.map((nav) => (
                         <>
-                            <li key={id} className="text-white hover:bg-slate-700 text-sm flex items-center gap-y-8 cursor-pointer p-1 rounded-md mx-3">
+                            <li key={nav.id} className="text-white hover:bg-slate-700 text-sm flex items-center gap-y-8 cursor-pointer p-1 rounded-md mx-3">
                                 <Link to={nav.link}>
                                         <i className={nav.icon}/>                                   
                                 </Link>      
