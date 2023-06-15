67875cf1fe7743930697910a905206a8a08541f0
lhotovy <lhotovy@centrum.cz>
2023-06-15 12:17:44 +0200

    Changed education infos

--- src/data/education.js
+++ src/data/education.js
@@ -9,7 +9,7 @@ export const education = [
         between: '2011 - 2016',
         grade: 'Mgr.',
         logo: muniLogo,
-        bio: "I obtained a masters degree in five year study programme covering wide range of legal fields.",
+        bio: "I obtained masters degree in the five year study programme Law and Legal Science at Masaryk University, covering wide range of legal fields.",
         bio_head: "Masaryk University - Faculty of Law",
         id: 1
     },
@@ -19,7 +19,7 @@ export const education = [
         between: '2021-2022',
         grade: 'LL.M.',
         logo: edu,
-        bio: "I obtained a professional LL.M. degree in a one year remote study programme covering bussiness law, negotiation, soft skills and other topics related to company management",
+        bio: "I obtained a professional LL.M. degree in the one year remote study programme Business law at EDU Effective Business School, covering bussiness law, negotiation, soft skills and other topics related to company management.",
         bio_head: "EDU Effective Bussiness School",
         id: 2
     },
@@ -29,8 +29,8 @@ export const education = [
         between: '2013',
         grade: 'Short-term study stay',
         logo: uib,
-        bio: "I studied for 5 months on Universitet i Bergen, Norway, via Erasmus exchange programme (part of my legal degree at Masaryk University).",
+        bio: "I studied for 5 months at Universitet i Bergen, Norway, via Erasmus exchange programme (part of my legal degree at Masaryk University).",
         bio_head: "Universitet i Bergen",
-        id:3
+        id: 3
     }
 ];
